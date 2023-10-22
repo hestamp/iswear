50fdeb276f6a78a8df89a373202ced0c8e178665
Tom Hestamp <tom@hestamp.me>
2023-10-22 19:55:49 +0300

    pwa. seo

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+// Install and activate the service worker
+self.addEventListener('install', (event) => {
+  // console.log('Service worker installed')
+})
+
+self.addEventListener('activate', (event) => {
+  // console.log('Service worker activated')
+})
+
+// // Fetch event handler
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(fetch(event.request))
+// })
